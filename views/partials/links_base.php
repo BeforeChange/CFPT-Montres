29bf68252a49f638fc4c7e2584e03e0a84291249
ErdiIbrhm <erdi.ibrhm@eduge.ch>
2026-02-12 10:13:16 +0100

    Ajoute de panier dans le links_base.php

--- views/partials/links_base.php
+++ views/partials/links_base.php
@@ -12,6 +12,12 @@
         </a>
     </li>
 
+    <li class="nav-item">
+        <a class="nav-link position-relative" href="cart.php">
+            <ion-icon name="cart-outline" style="font-size:22px;"></ion-icon>
+       </a>
+    </li>
+
 <?php else: ?>
 
     <li class="nav-item">
